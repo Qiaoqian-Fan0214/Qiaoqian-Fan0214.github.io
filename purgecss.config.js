d81a0c04204c9966be153d1800de454942ce2001
Qiaoqian-Fan0214 <yzhao642@connect.hkust-gz.edu.cn>
2026-06-05 18:54:37 +0800

    Keep visitor map styles after purge

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,5 +2,8 @@ module.exports = {
   content: ["_site/**/*.html", "_site/**/*.js"],
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
+  safelist: {
+    standard: [/^mapmyvisitors/, /^jvectormap/, "mpmvs"],
+  },
   skippedContentGlobs: ["_site/assets/**/*.html"],
 };
